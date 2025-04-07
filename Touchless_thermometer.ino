9022efee844030ae3cf0718b1cf9a83da5e11d38
Swarup Majumder <136871844+swarupm3@users.noreply.github.com>
2025-04-06 19:48:29 -0500

    Both cold and hot thresholds complete

--- Touchless_thermometer.ino
+++ Touchless_thermometer.ino
@@ -7,7 +7,10 @@
 #define segG 2
 #define digitOne 9
 #define digitTwo 10
-#define potentiometerPin A0
+#define digitThree 11
+#define digitFour 12
+#define potentiometerPinOne A0
+#define potentiometerPinTwo A1
 
 // segment encoding for digits 0-9
 const int digits[10][7] = {
@@ -38,14 +41,22 @@ void setup() {
   digitalWrite(digitOne, HIGH); // Off initially
   digitalWrite(digitTwo, HIGH);
 
+  pinMode(digitThree, OUTPUT);
+  pinMode(digitFour, OUTPUT);
+  digitalWrite(digitThree, HIGH); // Off initially
+  digitalWrite(digitFour, HIGH);
+
   // Potentiometer input
-  pinMode(potentiometerPin, INPUT);
+  pinMode(potentiometerPinOne, INPUT);
+  pinMode(potentiometerPinTwo, INPUT);
 }
 
 void displayDigit(int digit, int position) {
   // Turn off both digits before updating
   digitalWrite(digitOne, HIGH);
   digitalWrite(digitTwo, HIGH);
+  digitalWrite(digitThree, HIGH);
+  digitalWrite(digitFour, HIGH);
 
   // Set segment pins based on digit pattern (HIGH = ON for common cathode)
   for (int i = 0; i < 7; i++) {
@@ -57,21 +68,37 @@ void displayDigit(int digit, int position) {
     digitalWrite(digitOne, LOW);
   } else if (position == 2) {
     digitalWrite(digitTwo, LOW);
+  } else if (position == 3) {
+    digitalWrite(digitThree, LOW);
+  } else if (position == 4) {
+    digitalWrite(digitFour, LOW);
   }
 }
 
 void loop() {
   // Read potentiometer and map to 0–99
-  int potValue = analogRead(potentiometerPin);
-  int number = map(potValue, 0, 910, 0, 99);
+  int potValueOne = analogRead(potentiometerPinOne);
+  int numberOne = map(potValueOne, 0, 910, 0, 50);
 
   // Break number into tens and ones
-  int tens = number / 10;
-  int ones = number % 10;
+  int tensOne = numberOne / 10;
+  int onesOne = numberOne % 10;
+
+  // Read potentiometer and map to 0–99
+  int potValueTwo = analogRead(potentiometerPinTwo);
+  int numberTwo = map(potValueTwo, 0, 910, 0, 50);
+
+  // Break number into tens and ones
+  int tensTwo = numberTwo / 10;
+  int onesTwo = numberTwo % 10;
 
   // Multiplex display
-  displayDigit(tens, 1);
-  delay(5); // Reduce delay for smoother display
-  displayDigit(ones, 2);
-  delay(5);
+  displayDigit(tensOne, 1);
+  delay(4); // Reduce delay for smoother display
+  displayDigit(onesOne, 2);
+  delay(4);
+  displayDigit(tensTwo, 3);
+  delay(4); // Reduce delay for smoother display
+  displayDigit(onesTwo, 4);
+  delay(4);
 }
